571902395e305eb8012af5b676a210d30b3ee8a2
desslinga <fides.linga@mail.utoronto.ca>
2018-03-01 09:03:44 -0500

    stuff

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-(function() {
+function submenu_expand() {
+  $(".first-level").click(function () {
+    console.log("hey");
+    if ($(this).find(".submenu").hasClass("visible")) {
+      $(this).find(".submenu").removeClass("visible");
+    } else {
+      $(this).find(".submenu").addClass("visible");
+    }
+  });
+}
+$(document).ready(function() {
   "use strict";
-
+  submenu_expand();
   document.querySelector("#snt").addEventListener("click", function() {
     var sidenav = document.getElementById("sn");
     var mc = document.getElementById("mc");
@@ -11,6 +21,7 @@
       sidenav.style.display = "none";
       mc.classList.remove("nav-visible");
     }
-  });
 
-}());
+
+  });
+});
